5984874b2e74fb2c944e971eb2b076550e6984b7
U-RYOTA-THINK\RYOTA <RYOTA@RYOTA-THINK.(none)>
2014-08-16 11:04:15 +0900

    modified:   develop.py

--- scripts/scripts/develop.py
+++ scripts/scripts/develop.py
@@ -3,10 +3,9 @@ import os
 from scripts.hubot_script import *
 
 class Develop(HubotScript):
-    
-    @respond('message')
-    def message(self, message, matches):
-        return str(message)
+	@respond('message')
+	def message(self, message, matches):
+		return str(message)#{u'message': u'message', u'type': u'respond', u'room': u'bot_dev'}
 
 	@respond('matches')
 	def match(self,message,matches):
