4675ca61f8cf9bbc1f6a24a0b49fac9d30dea3c7
U-RYOTA-THINK\RYOTA <RYOTA@RYOTA-THINK.(none)>
2014-08-20 03:39:14 +0900

    modified:   scripts/develop.py

--- scripts/scripts/develop.py
+++ scripts/scripts/develop.py
@@ -19,13 +19,19 @@ class Develop(HubotScript):
 		response = urllib2.urlopen('https://script.google.com/macros/s/AKfycbww_pq15yfa6reDG0HIKfAcZssf61Vrq9DNJuVk87XD9_QNICI/exec')
 		data = []
 		data = json.loads(response.read())
-		diff = (datetime.date.today() - datetime.date(2014,8,7)).days
+		today = datetime.date.today()
+		day_key = str(today.year).zfill(4)+'/'+str(today.month).zfill(2)+'/'+str(today.day).zfill(2)
 		s = "Today's attendance\n"
 		alert = 'Fill this sheet immediately\nhttps://docs.google.com/spreadsheets/d/1KJ18JLS6JbZT6RgzfjF7-i3y6S8tpkSlcgDAAb4Rtyk/edit#gid=0\n'
+		for key in data[0].keys():
+			if day_key in key:
+				day_key = key
 		for row in data[4:]:
 			s += row.values()[0].split('@')[0] + ':'
-			s += row.values()[diff] + '\n'
-			if not row.values()[diff]:
+			if row[day_key]:
+				s += row[day_key]
+			else:
 				alert += '@'+row.values()[0].split('@')[1]+': '
+			s += '\n'
 		s += alert
 		return s
